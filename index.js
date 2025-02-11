04976f946f99909dfbbbc86ecf27b3eb3e1ee3dd
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-02-11 18:03:48 -0300

    feat: create middleware for json and routes

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import userRouter from "./routes/user.route.js";
 
+// Crea una instancia de la aplicacion de express
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hola Mundo");
-});
+// Middleware para enviar peticiones json
+app.use(express.json());
+
+// Middleware para enviar formularios a traves de HTML
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware para las rutas de usuarios
+app.use("/api/v1/users", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
+// Inicia el servidor
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
