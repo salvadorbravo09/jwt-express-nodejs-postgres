ace911df303c8d0863dae75bae51e52c7fb1fa91
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-02-12 17:24:21 -0300

    feat: create private route for profile

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/jwt.middleware.js";
 
 const router = Router();
 
+// Rutas publicas
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+// Rutas protegidas (verificacion del token)
+router.get("/profile", verifyToken, UserController.profile);
 
 export default router;
