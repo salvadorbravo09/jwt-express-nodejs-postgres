b7ddb7274be4aa13464774a7827d861c19af43f9
Salvador Bravo <sa.bravo0901@gmail.com>
2025-02-19 18:04:09 -0300

    feat: add middleware for verify if user is admin or veterinary

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -12,10 +12,28 @@ export const verifyToken = (req, res, next) => {
   token = token.split(" ")[1];
 
   try {
-    const { email } = jwt.verify(token, process.env.JWT_SECRET); // Verifica el token y extrae el email
+    const { email, role_id } = jwt.verify(token, process.env.JWT_SECRET); // Verifica el token y extrae el email
     req.email = email; // Guarda los datos del usuario para su uso en otras rutas
+    req.role_id = role_id;
     next(); // Llama a la siguiente funcion middleware si la verificacion esta OK
   } catch (error) {
     return res.status(400).json({ msg: "Token invalido" });
   }
+};
+
+export const verifyAdmin = (req, res, next) => {
+  // Verifica si el role_id es 1 (administrador)
+  if (req.role_id === 1) {
+    return next();
+  }
+  // Retorna error si el usuario no es administrador
+  return res.status(403).json({ error: "No autorizado, solo administradores" });
+};
+
+export const verifyVet = (req, res, next) => {
+  // Verifica si el role_id es 2 (veterinario) o si es role_id 1 (administrador)
+  if (req.role_id === 2 || req.role_id === 1) {
+    return next();
+  }
+  return res.status(403).json({ error: "No autorizado, solo veterinarios" });
 };
